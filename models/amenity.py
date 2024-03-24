28598a5834cbdb2195b08274d7ac06456368098e
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-24 04:32:35 +0200

    Add solution

--- models/amenity.py
+++ models/amenity.py
@@ -2,14 +2,24 @@
 '''
 Amenity
 '''
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+import models
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     '''Amenity:
 
     Attributes:
         name (str): amenity.
     '''
 
-    name = ''
+    if models.is_db:
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ''
+
+    def __init__(self, *args, **kwargs):
+        '''Init Amenity'''
+        super().__init__(*args, **kwargs)
--- models/review.py
+++ models/review.py
@@ -2,10 +2,13 @@
 '''
 Review
 '''
-from models.base_model import BaseModel
+
+from sqlalchemy import Column, String, ForeignKey
+import models
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     '''Review
 
     Attributes:
@@ -14,6 +17,16 @@ class Review(BaseModel):
         place_id (str): The Place.id.
     '''
 
-    text = ''
-    user_id = ''
-    place_id = ''
+    if models.is_db:
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """Init Review"""
+        super().__init__(*args, **kwargs)
