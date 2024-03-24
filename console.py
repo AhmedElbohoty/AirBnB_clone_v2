212bf7fc0c8037581ed42ff1ed6c0252660a639b
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-24 03:51:02 +0200

    Add solution

--- console.py
+++ console.py
@@ -26,6 +26,11 @@ class HBNBCommand(cmd.Cmd):
                  'City': City, 'Amenity': Amenity,
                  'Place': Place, 'Review': Review}
     prompt = '(hbnb) '
+    types = {
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
     def do_quit(self, _):
         '''Exit the program with 'quit' command'''
@@ -85,24 +90,88 @@ class HBNBCommand(cmd.Cmd):
         self.do_update(args)
         storage.save()
 
-    def do_update(self, arg):
-        '''Updates an instance based on the class name and id by adding
-        or updating attribute and save the change into the JSON file.
-        '''
-        inputs = arg.split()
-        is_valid = self.validate_input(
-            inputs, ['classname', 'id', 'attribute_name', 'attribute_value'])
+    def do_update(self, args):
+        """ Updates a certain object with new info """
+        c_name = c_id = att_name = att_val = kwargs = ''
 
-        if not is_valid:
+        # isolate cls from id/args, ex: (<cls>, delim, <id/args>)
+        args = args.partition(" ")
+        if args[0]:
+            c_name = args[0]
+        else:  # class name not present
+            print("** class name missing **")
+            return
+        if c_name not in HBNBCommand.__classes:  # class name invalid
+            print("** class doesn't exist **")
             return
 
-        objects = storage.all()
-        k = '{}.{}'.format(inputs[0], inputs[1])
-        obj = objects.get(k)
+        # isolate id from args
+        args = args[2].partition(" ")
+        if args[0]:
+            c_id = args[0]
+        else:  # id not present
+            print("** instance id missing **")
+            return
 
-        setattr(obj, inputs[2], inputs[3])
+        # generate key from class and id
+        key = c_name + "." + c_id
 
-        storage.save()
+        # determine if key is present
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        # first determine if kwargs or args
+        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) is dict:
+            kwargs = eval(args[2])
+            args = []  # reformat kwargs into list, ex: [<name>, <value>, ...]
+            for k, v in kwargs.items():
+                args.append(k)
+                args.append(v)
+        else:  # isolate args
+            args = args[2]
+            if args and args[0] == '\"':  # check for quoted arg
+                second_quote = args.find('\"', 1)
+                att_name = args[1:second_quote]
+                args = args[second_quote + 1:]
+
+            args = args.partition(' ')
+
+            # if att_name was not quoted arg
+            if not att_name and args[0] != ' ':
+                att_name = args[0]
+            # check for quoted val arg
+            if args[2] and args[2][0] == '\"':
+                att_val = args[2][1:args[2].find('\"', 1)]
+
+            # if att_val was not quoted arg
+            if not att_val and args[2]:
+                att_val = args[2].partition(' ')[0]
+
+            args = [att_name, att_val]
+
+        # retrieve dictionary of current objects
+        new_dict = storage.all()[key]
+
+        # iterate through attr names and values
+        for i, att_name in enumerate(args):
+            # block only runs on even iterations
+            if (i % 2 == 0):
+                att_val = args[i + 1]  # following item is value
+                if not att_name:  # check for att_name
+                    print("** attribute name missing **")
+                    return
+                if not att_val:  # check for att_value
+                    print("** value missing **")
+                    return
+                # type cast as necessary
+                if att_name in HBNBCommand.types:
+                    att_val = HBNBCommand.types[att_name](att_val)
+
+                # update dictionary with name, value pair
+                new_dict.__dict__.update({att_name: att_val})
+
+        new_dict.save()  # save updates to file
 
     def do_show(self, arg):
         '''Prints the string representation of an instance
