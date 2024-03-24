b7b51407201e6eec11ab4999b7c6b2952fffe697
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-24 04:34:05 +0200

    Add solution

--- models/place.py
+++ models/place.py
@@ -78,22 +78,26 @@ class Place(BaseModel, Base):
     if not models.is_db:
         @property
         def reviews(self):
-            '''getter attribute returns the list of Review instances'''
+            '''getter attribute returns Review'''
             from models.review import Review
-            review_list = []
-            all_reviews = models.storage.all(Review)
-            for review in all_reviews.values():
-                if review.place_id == self.id:
-                    review_list.append(review)
-            return review_list
+            result = []
+
+            revs = models.storage.all(Review)
+            for review in revs.values():
+                if review.place_id != self.id:
+                    continue
+                result.append(review)
+            return result
 
         @property
         def amenities(self):
-            '''getter attribute returns the list of Amenity instances'''
+            '''getter attribute returns Amenity'''
             from models.amenity import Amenity
-            amenity_list = []
-            all_amenities = models.storage.all(Amenity)
-            for amenity in all_amenities.values():
-                if amenity.place_id == self.id:
-                    amenity_list.append(amenity)
-            return amenity_list
+            res = []
+            amenities = models.storage.all(Amenity)
+
+            for amenity in amenities.values():
+                if amenity.place_id != self.id:
+                    return
+                res.append(amenity)
+            return res
