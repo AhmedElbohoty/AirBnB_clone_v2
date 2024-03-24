f5a5d25858c8fa2d8c3b03ff65ed05460d506924
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-25 01:41:58 +0200

    Add solution

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,7 +2,6 @@
 '''Unit tests for base model module'''
 import unittest
 from unittest.mock import patch
-from uuid import UUID
 from io import StringIO
 from console import HBNBCommand
 
@@ -16,4 +15,4 @@ class TestHBNBCommand(unittest.TestCase):
             HBNBCommand().onecmd("create User age=25")
 
             obj_id = f.getvalue().strip()
-            self.assertTrue(type(obj_id) == str)
+            self.assertTrue(type(obj_id) is str)
