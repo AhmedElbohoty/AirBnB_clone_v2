288f386ac69af6444d9e74afd44fe0c7b78921a0
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-03-25 03:36:00 +0200

    Add solution

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 """ Module for testing db storage"""
 import unittest
+import models
 
 
 class TestDBStorage(unittest.TestCase):
-    """ Class to test the db storage method """
+    """ Test """
+    @unittest.skipIf(not models.is_db, "DB storage")
+    def test_all_returns_dict(self):
+        """ Test """
+
+    @unittest.skipIf(not models.is_db, "DB storage")
+    def test_new(self):
+        """ Test """
+
+    @unittest.skipIf(not models.is_db, "DB storage")
+    def test_reload(self):
+        """ Test """
+
+    @unittest.skipIf(not models.is_db, "DB storage")
+    def test_save(self):
+        """ Test """
