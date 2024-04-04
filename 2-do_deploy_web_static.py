cd43e48693df801526bf9b17db817ce043d3df32
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-04-04 22:35:49 +0200

    Add solution

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ distributes an archive to your web servers, using the function do_deploy
 from os.path import exists
 from fabric.api import env, put, run
 
-env.hosts = ['54.160.70.107', '54.237.63.194']
+env.hosts = ["54.160.70.107", "54.237.63.194"]
 
 
 def do_deploy(archive_path):
@@ -20,26 +20,27 @@ def do_deploy(archive_path):
         return False
 
     try:
-        archive_name = archive_path.split('/')[-1]
-        archive_folder = "/data/web_static/releases/" + archive_name[:-4]
+        archive_name = archive_path.split("/")[-1]
+        archive_folder = "/data/web_static/releases/" + \
+            archive_name.split(".")[0]
 
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
 
         # Uncompress the archive to the folder
-        run(f"sudo mkdir -p {archive_folder}")
-        run(f"sudo tar -xzf /tmp/{archive_name} -C {archive_folder}")
+        run(f"mkdir -p {archive_folder}")
+        run(f"tar -xzf /tmp/{archive_name} -C {archive_folder}")
 
         # Delete the archive from the web server
-        run(f"sudo rm /tmp/{archive_name}")
+        run(f"rm /tmp/{archive_name}")
 
         # Delete the symbolic link /data/web_static/current from the web server
-        run(f"sudo mv {archive_folder}/web_static/* {archive_folder}")
-        run(f"sudo rm -rf {archive_folder}/web_static")
-        run("sudo rm -rf /data/web_static/current")
+        run(f"mv {archive_folder}/web_static/* {archive_folder}")
+        run(f"rm -rf {archive_folder}/web_static")
+        run("rm -rf /data/web_static/current")
 
         # Create a new the symbolic link /data/web_static/current on web server
-        run(f"sudo ln -s {archive_folder} /data/web_static/current")
+        run(f"ln -s {archive_folder}/ /data/web_static/current")
 
         return True
 
